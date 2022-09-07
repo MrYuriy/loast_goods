f1b797dad5921fd3c4f79cc9869d48e1c386318d
unknown <yuriybabiyk@gmail.com>
2022-09-07 21:04:28 +0300

    connect to front

--- lost_goods/settings.py
+++ lost_goods/settings.py
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = ["https://test-u-murex.vercel.app"]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
